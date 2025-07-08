38cb7158caa56007c465205f0f62f3a19c722c55
tymoneq <50627886+tymoneq@users.noreply.github.com>
2025-07-08 13:46:57 +0200

    learning js

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,11 @@
-alert('Hello World!')
+let currentResult = 0;
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+currentResult += add(10, currentResult);
+
+let calculationDescription = `Added 10 to the initial value. New result is ${currentResult}.`;
+
+outputResult(currentResult, calculationDescription);
