9e7c6d7362deba68f64c1219473165f1bcb8c1a6
tymoneq <50627886+tymoneq@users.noreply.github.com>
2025-07-10 08:28:44 +0200

    more js

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -7,43 +7,67 @@ function getUserInput() {
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-  logEntries.push(operator);
-  console.log(logEntries);
-
   outputResult(currentResult, calcDescription);
 }
+
+function writeToLog(operation, prevResult, number, result) {
+  let logEntry = {
+    operation: operation,
+    prevResult: prevResult,
+    number: number,
+    result: result,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
 
 function add() {
   const enteredNumber = getUserInput();
   currentResult += enteredNumber;
-
-  let logEntry = {
-    operation: "ADD",
-    prevResult: currentResult - enteredNumber,
-    number: enteredNumber,
-    result: currentResult,
-  };
-  logEntries.push(logEntry);
-
   createAndWriteOutput("+", currentResult - enteredNumber, enteredNumber);
+  writeToLog(
+    "ADD",
+    currentResult - enteredNumber,
+    enteredNumber,
+    currentResult
+  );
 }
 
 function subtract() {
   const enteredNumber = getUserInput();
   currentResult -= enteredNumber;
   createAndWriteOutput("-", currentResult + enteredNumber, enteredNumber);
+  writeToLog(
+    "SUBTRACT",
+    currentResult + enteredNumber,
+    enteredNumber,
+    currentResult
+  );
 }
 
 function multiply() {
   const enteredNumber = getUserInput();
   currentResult *= enteredNumber;
   createAndWriteOutput("*", currentResult / enteredNumber, enteredNumber);
+  writeToLog(
+    "MULTIPLY",
+    currentResult / enteredNumber,
+    enteredNumber,
+    currentResult
+  );
 }
 
 function divide() {
   const enteredNumber = getUserInput();
   currentResult /= enteredNumber;
   createAndWriteOutput("/", currentResult * enteredNumber, enteredNumber);
+  writeToLog(
+    "DIVIDE",
+    currentResult * enteredNumber,
+    enteredNumber,
+    currentResult
+  );
 }
 
 addBtn.addEventListener("click", add);
